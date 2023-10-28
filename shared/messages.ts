123d963404a34dd5553da0336c1491a82358289a
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-10-28 12:04:55 +0900

    change message to Zundamon-ish.

--- shared/messages.ts
+++ shared/messages.ts
@@ -1,13 +1,13 @@
 // app messages
-export const REVIEW_REQUEST_MESSAGE = 'Please review the following code.';
+export const REVIEW_REQUEST_MESSAGE = 'ずんだもん！コードレビューの依頼よ！';
 
 // prompt
 export const OPENAI_RESTORE_CODE_MESSAGE =
   'Diff file contents and original code before changes are as follows.\nPlease restore the code after changes based on this.';
 
 // slack reply messages
-export const SLACK_EMPTY_MESSAGE_REPLY = 'Messageを入力してください。';
-export const SLACK_START_REVIEW_MESSAGE = (fileName: string): string => `${fileName}のコードレビューを開始します。`;
+export const SLACK_EMPTY_MESSAGE_REPLY = 'おい、おまえ、メッセージをを入力するのだ。';
+export const SLACK_START_REVIEW_MESSAGE = (fileName: string): string => `${fileName}のコードレビューを開始するのだ。`;
 
 export const chatGPTRequestMessages: string[] = [
   'このコードにバグはありませんか？あれば教えてください',
