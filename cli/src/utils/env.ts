4444703e34bf752581040067677a07e19586282c
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-11-03 11:13:56 +0900

    Corrected incorrect path for reading .env file.

--- cli/src/utils/env.ts
+++ cli/src/utils/env.ts
@@ -1,13 +1,22 @@
 import { Env } from '../types';
 import { load } from 'ts-dotenv';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { ENV_FILE_NAME } from '../constants';
 
 export const getEnv = (): Env => {
   try {
-    const env = load({
-      SLACK_BOT_TOKEN: String,
-      SLACK_BOT_ID: String,
-      SLACK_CHANNEL_ID: String,
-    });
+    const currentFileUrl = import.meta.url;
+    const appDirectory = path.dirname(fileURLToPath(currentFileUrl));
+    const envFilePath = path.join(appDirectory, ENV_FILE_NAME);
+    const env = load(
+      {
+        SLACK_BOT_TOKEN: String,
+        SLACK_BOT_ID: String,
+        SLACK_CHANNEL_ID: String,
+      },
+      envFilePath,
+    );
     if (!env.SLACK_BOT_TOKEN || !env.SLACK_BOT_ID || !env.SLACK_CHANNEL_ID) {
       console.error(
         'SLACK_BOT_TOKEN and/or SLACK_BOT_ID and/or SLACK_CHANEL_ID are not set in the environment variables',
