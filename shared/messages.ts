eb00883abaa0ebc3615f2ee4e8fc719b3560317c
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-11-05 07:55:10 +0900

    Change Restore code message to a template literal.

--- shared/messages.ts
+++ shared/messages.ts
@@ -2,8 +2,8 @@
 export const REVIEW_REQUEST_MESSAGE = 'ずんだもん！コードレビューの依頼よ！';
 
 // prompt
-export const OPENAI_RESTORE_CODE_MESSAGE =
-  'Diff file contents and original code before changes are as follows.\nPlease restore the code after changes based on this.';
+export const OPENAI_RESTORE_CODE_MESSAGE = `Diff file contents and original code before changes are as follows.
+  Please restore the code after changes based on this.`;
 
 // slack reply messages
 export const SLACK_EMPTY_MESSAGE_REPLY = 'おい、おまえ、メッセージをを入力するのだ。';
